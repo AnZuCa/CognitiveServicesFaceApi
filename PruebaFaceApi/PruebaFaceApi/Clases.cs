34580efbc38120b689b754fc39c6f19a0f428857
ANDRES ZUÑIGA CALDERON <andres.zuniga.calderon@est.una.ac.cr>
2020-06-17 18:16:44 -0500

    Proyecto

---
+++ PruebaFaceApi/PruebaFaceApi/Clases.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PruebaFaceApi
+{
+    public class FaceDetectResponse
+    {
+        public string FaceId { get; set; }
+        public FaceAttributes FaceAttributes { get; set; }
+        public FaceLandmarks FaceLandmarks { get; set; }
+        public FaceRectangle FaceRectangle { get; set; }
+
+
+    }
+    public class FaceRectangle
+    {
+        public int Height { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Width { get; set; }
+
+    }
+
+    public class FaceLandmarks
+    {
+        public FeatureCoordinate EyebrowLeftInner { get; set; }
+        public FeatureCoordinate EyebrowLeftOuter { get; set; }
+        public FeatureCoordinate EyebrowRightInner { get; set; }
+        public FeatureCoordinate EyebrowRightOuter { get; set; }
+        public FeatureCoordinate EyeLeftBottom { get; set; }
+
+        public FeatureCoordinate EyeLeftInner { get; set; }
+
+        public FeatureCoordinate EyeLeftOuter { get; set; }
+
+        public FeatureCoordinate EyeLeftTop { get; set; }
+        public FeatureCoordinate EyeRightBottom { get; set; }
+
+        public FeatureCoordinate EyeRightInner { get; set; }
+        public FeatureCoordinate EyeRightOuter { get; set; }
+
+        public FeatureCoordinate EyeRightTop { get; set; }
+        public FeatureCoordinate MouthLeft { get; set; }
+        public FeatureCoordinate MouthRight { get; set; }
+        public FeatureCoordinate NoseLeftAlarOutTip { get; set; }
+        public FeatureCoordinate NoseLeftAlarTop { get; set; }
+        public FeatureCoordinate NoseRightAlarOutTip { get; set; }
+        public FeatureCoordinate NoseRightAlarTop { get; set; }
+        public FeatureCoordinate NoseRootLeft { get; set; }
+        public FeatureCoordinate NoseRootRight { get; set; }
+        public FeatureCoordinate NoseTip { get; set; }
+
+        public FeatureCoordinate PupilLeft { get; set; }
+        public FeatureCoordinate PupilRight { get; set; }
+
+        public FeatureCoordinate UnderLipBottom { get; set; }
+
+        public FeatureCoordinate UnderLipTop { get; set; }
+        public FeatureCoordinate UpperLipBottom { get; set; }
+        public FeatureCoordinate UpperLipTop { get; set; }
+    }
+    public class FeatureCoordinate
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+    }
+    public class FaceAttributes
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Glasses Glasses { get; set; }
+        public double Age { get; set; }
+        public FacialHair FacialHair { get; set; }
+        public string Gender { get; set; }
+        public HeadPose HeadPose { get; set; }
+        public double Smile { get; set; }
+
+    }
+    public enum Glasses
+    {
+        NoGlasses = 0,
+        Sunglasses = 1,
+        ReadingGlasses = 2,
+        SwimmingGoggles = 3
+    }
+    public class HeadPose
+    {
+        public double Pitch { get; set; }
+        public double Roll { get; set; }
+        public double Yaw { get; set; }
+
+    }
+    public class FacialHair
+    {
+        public double Beard { get; set; }
+        public double Moustache { get; set; }
+        public double Sideburns { get; set; } //patillas
+
+    }
+
+    public class FaceVerifyResponse
+    { 
+     public double Confidence { get; set; }
+      public bool IsIdentical { get; set; }
+    }
+
+}
---
+++ PruebaFaceApi/PruebaFaceApi/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+namespace PruebaFaceApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data1, data2, resp1, resp2;
+            String url = "https://eastus2.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&returnFaceAttributes=age,gender";
+            FaceDetectResponse fd1, fd2;
+            using (var q = new WebClient())
+            {
+
+
+                q.Headers.Add(HttpRequestHeader.ContentType, "application/octet-stream");
+                q.Headers.Add("Ocp-Apim-Subscription-Key", "42995da1c53e42288c2fcda472fc272a");
+                data1 = File.ReadAllBytes("C:\\Users\\hp\\Desktop\\Rostro1.jpg");
+
+                resp1 = q.UploadData(url, data1);
+                string str1 = Encoding.UTF8.GetString(resp1);
+                fd1 = JsonConvert.DeserializeObject<FaceDetectResponse[]>(str1)[0];
+
+            }
+            using (var q = new WebClient())
+            {
+                //Se especifica si se envia un url con Json o un conjunto de bytes, en este caso conjunto de bytes
+                q.Headers.Add(HttpRequestHeader.ContentType, "application/octet-stream");
+                //Key de Face Api en azure Cognitive services
+                q.Headers.Add("Ocp-Apim-Subscription-Key", "42995da1c53e42288c2fcda472fc272a");
+                //Localización de la imagen
+                data2 = File.ReadAllBytes("C:\\Users\\hp\\Desktop\\Rostro2.jpg");
+                //Se envian los datos y se recive la respuesta
+                resp2 = q.UploadData(url, data2);
+                //Decodificamos los datos en formato UTF8
+                String str2 = Encoding.UTF8.GetString(resp2);
+                fd2 = JsonConvert.DeserializeObject<FaceDetectResponse[]>(str2)[0];
+            }
+            using (var q = new WebClient())//verifico si las dos imagenes son la misma persona
+            {
+                q.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                q.Headers.Add("Ocp-Apim-Subscription-Key", "42995da1c53e42288c2fcda472fc272a");
+                url = "https://eastus2.api.cognitive.microsoft.com/face/v1.0/verify";
+                string json = "{\"faceId1\":\"" + fd1.FaceId + "\", \"faceId2\":\"" + fd2.FaceId + "\" }";
+                String str=q.UploadString(url, json);
+                FaceVerifyResponse resp3 = JsonConvert.DeserializeObject<FaceVerifyResponse>(str);
+
+                if (resp3.IsIdentical)
+                {
+                    Console.WriteLine("Las dos fotos son de la misma persona");
+                    Console.WriteLine("Coincidencia en las dos fotos:"+resp3.Confidence);
+                }
+                else {
+                    Console.WriteLine("Las dos fotos son de diferente persona");
+                    Console.WriteLine("Coincidencia en las dos fotos:" + resp3.Confidence);
+                }
+
+            }
+            Console.ReadLine();
+            }
+    }
+}
